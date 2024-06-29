1ed5a8c5f535a02bfa4472413aacb79eabb4c195
fernandoggaitan <fernandoggaitan@gmail.com>
2024-06-29 19:11:59 -0300

    refreshToken

--- src/routes/usuarioRoute.js
+++ src/routes/usuarioRoute.js
@@ -8,5 +8,6 @@ const usuarioController = require("../controllers/usuarioController");
 router.post("/register", usuarioController.register);
 router.post("/login", usuarioController.login);
 router.get("/welcome", requireAuth, usuarioController.welcome);
+router.get('/refresh-token', usuarioController.refreshToken);
 
 module.exports = router;
